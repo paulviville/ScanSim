9913a9a7c814d3afede83361804e75cbc001a60d
paulviville <paul.viville@gmail.com>
2024-03-01 21:37:37 +0100

    grid raycast done

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import * as THREE from './three.module.js';
 import { computeBoundsTree, disposeBoundsTree, acceleratedRaycast, MeshBVHHelper } from './bhv.js';
 import { GLTFLoader } from './GLTFLoader.js';
 import { OrbitControls } from './CMapJS/Libs/OrbitsControls.js';
+import { GUI } from './CMapJS/Libs/dat.gui.module.js';
+import IncidenceGraph from './CMapJS/CMap/IncidenceGraph.js';
+import Renderer from './CMapJS/Rendering/Renderer.js';
+
 
 // Add the extension functions
 THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
@@ -43,23 +47,23 @@ window.addEventListener('resize', function() {
     camera.updateProjectionMatrix();
 });
 
-
+let mesh;
 const loader = new GLTFLoader();
 loader.load( './LeePerrySmith.glb', function ( gltf ) {
 
-	const mesh = gltf.scene.children[ 0 ];
+	mesh = gltf.scene.children[ 0 ];
 	// mesh.material.wireframe = true;
 	mesh.scale.set(0.05, 0.05, 0.05)
 
 	mesh.geometry.computeBoundsTree();
 	console.log(mesh)
 
-	let bvhHelper = new MeshBVHHelper(mesh)
-	bvhHelper.color.set( 0xE91E63 );
-	bvhHelper.depth = 20
-	bvhHelper.update()
-	console.log(bvhHelper)
-	scene.add(bvhHelper)
+	// let bvhHelper = new MeshBVHHelper(mesh)
+	// bvhHelper.color.set( 0xE91E63 );
+	// bvhHelper.depth = 20
+	// bvhHelper.update()
+	// console.log(bvhHelper)
+	// scene.add(bvhHelper)
 
 	scene.add(mesh);
 } );
@@ -69,7 +73,7 @@ raycaster.setFromCamera(new THREE.Vector2(0, 0), camera)
 
 console.log(raycaster)
 
-const lidarPos = new THREE.Vector3(-1, 0, 1);
+const lidarPos = new THREE.Vector3(-0.6, 0, 1);
 lidarPos.normalize().multiplyScalar(0.7);
 /// TODO create position from angle Z lidar/origin 
 
@@ -94,9 +98,116 @@ lidar1Helper.update();
 scene.add(lidar1Helper)
 
 
+console.log(raycaster)
+// console.log(raycaster.intersectObject(mesh))
+
+window.raycaster = raycaster
 
 window.helper = lidar0Helper
 window.lidar0 = lidar0
+
+
+const pointCloud = new IncidenceGraph()
+pointCloud.createEmbedding(pointCloud.vertex)
+const pointCloudPosition = pointCloud.addAttribute(pointCloud.vertex, "position");
+
+const pointCloudRenderer = new Renderer(pointCloud);
+pointCloudRenderer.vertices.create({size: 0.0025}).addTo(scene);
+console.log(pointCloudRenderer.vertices.mesh)
+
+const pointCloud1 = new IncidenceGraph()
+pointCloud1.createEmbedding(pointCloud1.vertex)
+const pointCloud1Position = pointCloud1.addAttribute(pointCloud1.vertex, "position");
+
+const pointCloud1Renderer = new Renderer(pointCloud1);
+pointCloud1Renderer.vertices.create({size: 0.0025, color: new THREE.Color(0x00FF00)}).addTo(scene);
+console.log(pointCloud1Renderer.vertices.mesh)
+
+
+function addPoint(p) {
+	const v = pointCloud.addVertex();
+	pointCloudPosition[v] = p;
+}
+
+function addPoint1(p) {
+	const v = pointCloud1.addVertex();
+	pointCloud1Position[v] = p;
+
+}
+
+
+function updatePoints() {
+	pointCloudRenderer.vertices.update();
+	pointCloud1Renderer.vertices.update();
+}
+
+const guiParams = {
+	pointer: new THREE.Vector2,
+
+	castRay: function() {
+		raycaster.setFromCamera(this.pointer, lidar0);
+		const intersections = raycaster.intersectObject(mesh);
+
+		if(intersections[0]) {
+			addPoint(intersections[0].point.clone());
+		}
+		else {
+			addPoint(raycaster.ray.origin.clone().addScaledVector(raycaster.ray.direction, 2))
+		}
+	},
+
+	step: 0.015,
+	castRayGrid: function () {
+		for(let i = -1; i < 1; i += this.step) {
+			for(let j = -1; j < 1; j += this.step) {
+				this.pointer.set(i, j);
+				this.castRay()
+			}	
+		}
+
+		updatePoints()
+	},
+
+	castRay1: function() {
+		raycaster.setFromCamera(this.pointer, lidar1);
+		const intersections = raycaster.intersectObject(mesh);
+
+		if(intersections[0]) {
+			addPoint1(intersections[0].point.clone());
+		}
+		else {
+			addPoint1(raycaster.ray.origin.clone().addScaledVector(raycaster.ray.direction, 2))
+		}
+	},
+
+	castRayGrid1: function () {
+		for(let i = -1; i < 1; i += this.step) {
+			for(let j = -1; j < 1; j += this.step) {
+				this.pointer.set(i, j);
+				this.castRay1()
+			}	
+		}
+
+		updatePoints()
+	}
+
+	// function 
+};
+
+const gui = new GUI();
+gui.add(guiParams, "castRay")
+gui.add(guiParams, "castRayGrid")
+gui.add(guiParams, "castRayGrid1")
+gui.add(guiParams, "step", 0.01, 0.1, 0.005);
+gui.add(guiParams.pointer, "x", -1, 1, 0.05);
+gui.add(guiParams.pointer, "y", -1, 1, 0.05);
+
+
+
+
+
+
+
 
 function update (t)
 {
